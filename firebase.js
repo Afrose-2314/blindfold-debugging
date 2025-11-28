231f9bc822439ec90da783017c4e6a7c7e6d1afe
Afrose <12851bca2023@princescience.in>
2025-11-28 20:00:48 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+apiKey: "YOUR_KEY",
+authDomain: "YOUR_PROJECT.firebaseapp.com",
+databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+projectId: "YOUR_PROJECT",
+storageBucket: "YOUR_PROJECT.appspot.com",
+messagingSenderId: "",
+appId: ""
+};
+firebase.initializeApp(firebaseConfig);
