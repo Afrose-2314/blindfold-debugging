6e9cc7ed76524f30e2a13cedbe8a73dd3e534004
Afrose <12851bca2023@princescience.in>
2025-11-28 21:40:46 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,16 @@
+// ----------------------------
+// Firebase Config — Blindfold Debugging
+// ----------------------------
 const firebaseConfig = {
   apiKey: "AIzaSyDJrMOF2P76MTCw0VOG5B6EyrubuNplTBo",
   authDomain: "blindfold-debugging.firebaseapp.com",
+  databaseURL: "https://blindfold-debugging-default-rtdb.firebaseio.com",
   projectId: "blindfold-debugging",
   storageBucket: "blindfold-debugging.firebasestorage.app",
   messagingSenderId: "425664674258",
   appId: "1:425664674258:web:3f80e3ae7083d40b0d300b",
   measurementId: "G-3251SXPYC8"
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
