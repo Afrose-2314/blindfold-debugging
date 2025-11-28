ce43be98e492a65c9dddeee3878922e864b1995e
Afrose <12851bca2023@princescience.in>
2025-11-28 20:02:34 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,19 @@
+const L1 = [
+{
+wrong:`a=5
+if a>3 print(a)`,
+correct:`a=5
+if a>3: print(a)`
+},
+{
+wrong:`for i in range(3) print(i)`,
+correct:`for i in range(3): print(i)`
+},
+{
+wrong:`def add(x y): return x+y`,
+correct:`def add(x, y): return x+y`
+}
+];
+
+
+const L2 = [...];
